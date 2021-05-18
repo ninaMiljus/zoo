13ea12d98fbafe9cf84b0ec94242ceb10d71b127
ninaMiljus <nina.miljus@gmail.com>
2021-05-18 20:13:47 +0200

    Zadaci 6 i 7

--- src/components/AnimalList.vue
+++ src/components/AnimalList.vue
@@ -17,15 +17,25 @@
         <input class="form-control" name="dateOfBirth" type="text" v-model="newAnimal.dateOfBirth"/>
       </div>
 
+      <div class="form-group">
+        <label for="sector.name" class="form-control">Sector:</label>
+        <select name="sector.name">
+            <option value="vodene">Vodene</option>
+            <option value="kopno">Kopno</option>
+            <option value="ptice">Ptice</option>
+        </select>
+      </div>
       <button class="btn btn-primary">Add Animal</button>
-    </form>
-    <br>
+       </form>
+    <br/>
     <table class="table">
       <thead>
         <tr>
           <th>Species</th>
           <th>Name</th>
           <th>Date of birth</th>
+          <th>Sector name</th>
+          <th>Sector surface</th>
           <th>&nbsp;</th>
         </tr>
       </thead>
@@ -34,6 +44,8 @@
           <td>{{ animal.species }}</td>
           <td>{{ animal.name }}</td>
           <td>{{ animal.dateOfBirth ? animal.dateOfBirth : 'Nepoznat' }}</td>
+            <td>{{ animal.sector.name }}</td>
+          <td>{{ animal.sector.surface }}</td>
         <td class="text-right">
         <button class="btn btn-danger btn-sm" @click="remove(animal)">Remove</button>
           <button class="btn btn-default btn-sm" @click="moveToTop(animal)">Move to top</button>
@@ -41,39 +53,66 @@
         </tr>
       </tbody>
     </table>
+    <table class="table">
+      <thead>
+        <th>Sector name</th>
+        <th>Surface</th>
+        <th>&nbsp;</th>
+      </thead>
+      <tbody>
+        <tr v-for="(sector, key) in sectors" :key="key">
+          <td>{{ sector.name }}</td>
+          <td>{{ sector.surface }}</td>
+          <td class="text-right">
+            <button class="btn btn-default btn-sm" @click="showAnimalsBySector(sector)">View list of animals</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
+const sectors = [
+  { name: 'Vodene', surface: 'Voda' },
+  { name: 'Kopnene', surface: 'Kopno' },
+  { name: 'Ptice', surface: 'Kavez'}
+];
 export default {
   name: 'AnimalList',
   data() {
     return {
+        sectors: sectors,
       animals: [
         {
           species: 'Dog',
           name: 'Dzeki',
-          dateOfBirth: ''
+          dateOfBirth: '',
+          sector: sectors[1]
         },
         {
           species: 'Cat',
           name: 'Cile',
-          dateOfBirth: ''
+          dateOfBirth: '',
+           sector: sectors[1]
         },
         {
           species: 'Parot',
           name: 'Zuti',
-          dateOfBirth: '24-02-2015'
+          dateOfBirth: '24-02-2015',
+           sector: sectors[2]
         },
         {
           species: 'Fish',
           name: 'Zlatna',
-          dateOfBirth: '08-08-2016'
+          dateOfBirth: '08-08-2016',
+           sector: sectors[0]
         },
         {
           species: 'Horse',
           name: 'Ridji',
-          dateOfBirth: '12-09-2009'
+          dateOfBirth: '12-09-2009',
+           sector: sectors[1]
         }
       ],
       newAnimal: {}
@@ -81,17 +120,26 @@ export default {
   },
         methods: {
             remove(animal) {
-            const index = this.animals.indexOf(animal);
-            this.animals.splice(index, 1);
+                const index = this.animals.indexOf(animal);
+                this.animals.splice(index, 1);
             },
-        moveToTop(animal) {
-        this.remove(animal);
-        this.animals.unshift(animal);
-        },
-        addAnimal() {
-        this.animals.push(this.newAnimal);
-        this.newAnimal = {};
+            moveToTop(animal) {
+                this.remove(animal);
+                this.animals.unshift(animal);
+            },
+            addAnimal() {
+                this.animals.push(this.newAnimal);
+                this.newAnimal = {};
+            },
+            showAnimalsBySector(sector) {
+                const animalsList=[];
+                    this.animals.forEach(animal => {
+                        if (animal.sector && animal.sector.name === sector.name) {
+                        animalsList.push(`${animal.name} ${animal.species}`);
                     }
+                });
+                alert(animalsList.toString());
+            }
         }
 };
 </script>
